07dc676354c9c1027c2307da1eeb00807537e406
Samuel Zaniewski <zaniewss@gmatc.matc.edu>
2021-03-04 13:55:06 -0800

    Completed Midterm Project 1

---
+++ samuel-zaniewski-p1/script.js
@@ -0,0 +1,6 @@
+var user = 'Mike'
+var salutaion = "Whats up, "
+var greeting =  salutaion + user + "Here is my website!"
+var greetinEl = document.getElementById('greeting')
+
+greetingEl.textContent = greeting;
