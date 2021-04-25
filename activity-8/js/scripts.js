0f95dd58acc1d7d1117978ea48b26ed20ecd9907
zaniewss <zaniewss@gmatc.matc.edu>
2021-04-25 11:11:05 -0500

    Completed Activity 8

---
+++ activity-8/js/scripts.js
@@ -0,0 +1,53 @@
+function openModal(){
+    var modal = get('modal-dialog');
+    var backdrop = get('modal-backdrop')
+
+    modal.classList.add('visible');
+    backdrop.classList.add('visible');
+}
+
+function closeModal(){
+    var title = get('edit-title-text');
+    var text = get('edit-content-text');
+    var modal = get('modal-dialog');
+    var backdrop = get('modal-backdrop');
+
+    //Clear text
+    title.value = '';
+    text.value = '';
+
+    //Hide modal
+    modal.classList.remove("visible");
+    backdrop.classList.remove('visible');
+}
+
+function saveContent(){
+    var title = get('edit-title-text');
+    var text = get('edit-content-text');
+    var content = get('display-content');
+
+    // Create Content elements
+    var newTitle = document.createElement('h2');
+    var newTitleText = document.createTextNode(title.value);
+    var newContent = document.createElement('p');
+    var newContentText = document.createTextNode(text.value);
+
+    // Add Element
+    newTitle.appendChild(newTitleText);
+    newContent.appendChild(newContentText);
+    content.appendChild(newTitle);
+    content.appendChild(newContent);
+
+    closeModal();
+}
+
+//Wire up event handlers
+window.addEventListener('load', function(){
+    var newButton = get('new-button');
+    var cancelButton = get('cancel-button');
+    var saveButton = get('save-button');
+
+    newButton.addEventListener('click', openModal);
+    cancelButton.addEventListener('click', closeModal);
+    saveButton.addEventListener('click', saveContent);
+})
